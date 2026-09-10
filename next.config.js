5fccba79144fd903aba49982b200555fd04fa838
kpzaolod6000 <kpzaolod6000@gmail.com>
2026-09-10 14:18:28 -0500

    fix: configurar basePath y assetPrefix para cargar estilos tailwind

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Debe coincidir exactamente con el nombre de tu repositorio en GitHub:
+  // Debe coincidir exactamente con el nombre de tu repositorio:
   basePath: '/project2026-CodingChallenge-frontend',
+  assetPrefix: '/project2026-CodingChallenge-frontend/',
   images: {
     unoptimized: true,
   },
